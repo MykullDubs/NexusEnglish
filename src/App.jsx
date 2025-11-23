fce557f170dcd1ed320c7464a7a7c60d422b3090
MykullDubs <142657467+MykullDubs@users.noreply.github.com>
2025-11-22 19:34:15 -0600

    Update Firebase configuration in App.jsx
    Removed placeholder comments for Firebase configuration.

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ import {
   doc, onSnapshot, query 
 } from "firebase/firestore";
 
-// --- 1. PASTE YOUR FIREBASE CONFIG HERE ---
   const firebaseConfig = {
     apiKey: "AIzaSyCfjzFY_yZQv66hF_Ob9-wHk1klKBe5rHw",
     authDomain: "nexusenglish-3e9c3.firebaseapp.com",
@@ -26,7 +25,6 @@ import {
     appId: "1:18259366717:web:5cd2e6239f58b18b9f6b54",
     measurementId: "G-YX9PTFX8G5"
   };
-
 // --- 2. Initialize Firebase ---
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
